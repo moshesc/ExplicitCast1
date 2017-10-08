d7189ee7a2135f192a5ed1d183da201b5472087d
moshesc <beepbeep9860@gmail.com>
2017-10-08 17:55:08 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitCast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long longNumber = 50918309109;
+            int intNumber = (int)longNumber;
+            Console.WriteLine(intNumber);
+        }
+    }
+}
